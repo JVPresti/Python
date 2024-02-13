8e1a4f75458c7fcf60dc82a3c754bcbff7e3383b
Jorge <nanojorgevazquez123@gmail.com>
2024-02-13 08:11:05 -0800

    Practica1

---
+++ Practica1.py
@@ -0,0 +1,24 @@
+# Jorge Vazquez
+# OnlyCodes
+
+'''
+import math
+
+print(f"{math.pi}")
+print(f"{math.pi:.4f}")
+
+x=2
+b=3
+c= 4
+
+z= x**2  + 3*b + c 
+print(z)
+
+a=16-2*5//3+1
+print(a)
+'''
+
+precio = 34.96
+descuento = precio * 0.15
+precioDescuento = precio - descuento
+print(f"EL precio con descuento es: {precioDescuento:.2f}")
