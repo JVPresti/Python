bb26b7832694f8cfe4833a4d507edda9b436f672
JVPresti <nanojorgevazquez123@gmail.com>
2024-02-27 08:20:05 -0800

    xd

---
+++ nose.py
@@ -0,0 +1,13 @@
+n1=47
+n2=95
+n3=88
+n4=73
+n5=88
+n6=84
+
+mayor= max(n1,n2,n3,n4,n5,n6)
+menor= min(n1,n2,n3,n4,n5,n6)
+rango= mayor-menor
+print("El menor es", menor)
+print("El mayor es", mayor)
+print("El rango es", rango-1, " - ", mayor)
